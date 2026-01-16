0b017d58fcf77c2ba703da7e11ff016738234f79
Metadon-svg <krisevicu@gmail.com>
2026-01-16 22:28:21 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,9 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        
+        // Добавьте эту строку для поддержки vector drawables
+        vectorDrawables.useSupportLibrary = true
     }
 
     buildTypes {
